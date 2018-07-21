019a5da49561c4686302b917641ba5b657a5d850
Prasad Bhagwat <prasad.bhagwat14k@gmail.com>
2018-07-20 23:07:30 -0700

    Betweenness scala code update

--- Prasad_Bhagwat_Betweenness.scala
+++ Prasad_Bhagwat_Betweenness.scala
@@ -11,7 +11,8 @@ object Betweenness{
     var num_shortest_path_dict  = Map[Int, Int]()
     var parents_dict            = Map[Int, Set[Int]]()
     // Dictionary initializations
-    for (user <- user_graph.keys){
+    for (user <- user_graph.keys)
+    {
       level_dict             += (user -> Int.MaxValue)
       num_shortest_path_dict += (user -> 0)
       parents_dict           += (user -> Set())
@@ -21,21 +22,26 @@ object Betweenness{
     num_shortest_path_dict += (root -> 1)
     var bfs_queue          = List(root)
 
-    while (bfs_queue.nonEmpty){
+    while (bfs_queue.nonEmpty)
+    {
       val current_node = bfs_queue.last
       bfs_queue        = bfs_queue.dropRight(1)
       val nodes_set    = user_graph(current_node).toSet
-      for (next_node <- nodes_set){
-        if (level_dict(next_node) == Int.MaxValue){
+      for (next_node <- nodes_set)
+      {
+        if (level_dict(next_node) == Int.MaxValue)
+        {
           level_dict   += (next_node -> (level_dict(current_node) + 1))
           // Putting next_node node at first location of visited Queue
           bfs_queue    = next_node::bfs_queue
         }
         // Getting shortest paths till root of the node
-        if (level_dict(next_node) == level_dict(current_node) + 1){
+        if (level_dict(next_node) == level_dict(current_node) + 1)
+        {
           num_shortest_path_dict += (next_node -> (num_shortest_path_dict(next_node) + num_shortest_path_dict(current_node)))
           var intermediate_set = parents_dict(next_node)
-          if (current_node != Int.MaxValue){
+          if (current_node != Int.MaxValue)
+	  {
             intermediate_set     += current_node
             parents_dict         += (next_node -> intermediate_set)
           }
@@ -54,35 +60,41 @@ object Betweenness{
     val max_depth         = level_dict.values.max
     var betweenness_dict  = Map[(Int, Int), Double]()
     // Looping from Max Depth to 0 for calculating betweenness
-    for (i <- max_depth to 1 by -1) {
-      val nodes_below     = level_dict.filter(_._2 == i)
-      for (node_below <- nodes_below.keys) {
-        val nodes_up       = parents_dict(node_below)
-        val children_nodes = corated_users_dict(node_below).toSet.diff(nodes_up)
-        val children_set   = children_nodes.filter(x => level_dict(x) > i)
+    for (i <- max_depth to 1 by -1) 
+    {
+      val nodes_below     	    = level_dict.filter(_._2 == i)
+      for (node_below <- nodes_below.keys) 
+      {
+        val nodes_up       	    = parents_dict(node_below)
+        val children_nodes          = corated_users_dict(node_below).toSet.diff(nodes_up)
+        val children_set            = children_nodes.filter(x => level_dict(x) > i)
         val nodes_up_shortest_paths = new ListBuffer[Int]()
-        for (x <- nodes_up){
-          nodes_up_shortest_paths += num_shortest_path_dict(x)
+        for (x <- nodes_up)
+        {
+          nodes_up_shortest_paths   += num_shortest_path_dict(x)
         }
         val num_shortest_paths  = nodes_up_shortest_paths.sum
         // If current node is a leaf node direct split the node value based on number of shortest paths till parent
-        if (children_set.isEmpty) {
+        if (children_set.isEmpty) 
+        {
           val result_dict  = nodes_up.map(node_up => {
             ((node_up, node_below), num_shortest_path_dict(node_up).toDouble / num_shortest_paths)
           }).toMap
           betweenness_dict = betweenness_dict ++ result_dict
         }
         // If current node is not a leaf node split the node value based on number of shortest paths till parent considering children credits
-        else {
+        else 
+        {
           val children_path_credit = new ListBuffer[Double]()
-          for (x <- children_set){
+          for (x <- children_set)
+          {
             children_path_credit   += betweenness_dict.getOrElse((node_below, x), 1.toDouble)
           }
-          val children_credit = children_path_credit.sum
-          val result_dict     = nodes_up.map(node_up => {
+          val children_credit      = children_path_credit.sum
+          val result_dict          = nodes_up.map(node_up => {
             ((node_up, node_below), ((1.toDouble + children_credit) * num_shortest_path_dict(node_up).toDouble) / num_shortest_paths)
           }).toMap
-          betweenness_dict    = betweenness_dict ++ result_dict
+          betweenness_dict         = betweenness_dict ++ result_dict
         }
       }
     }
@@ -102,28 +114,32 @@ object Betweenness{
     val output_file         = new PrintWriter(new File(output_file_name))
 
     // Creating Spark Context
-    val spark_config    = new SparkConf()
-    val spark_context   = new SparkContext(spark_config)
+    val spark_config        = new SparkConf()
+    val spark_context       = new SparkContext(spark_config)
     spark_context.setLogLevel("WARN")
 
     // Reading input training data file and extracting header
-    val input           = spark_context.textFile(input_file).filter(x => ! x.contains("userId"))
-    val input_data      = input.map( x => {
+    val input               = spark_context.textFile(input_file).filter(x => ! x.contains("userId"))
+    val input_data          = input.map( x => {
       val y = x.split(",")
       (y(0).toInt, y(1).toInt)
     })
 
     // Creating RDD of tuples like (user, movie) and dictionary of user: set(movies)
-    val user_movie_RDD          = input_data.map(x => (x._1, x._2)).groupByKey().filter(x => x._2.size >= 9)
-    val user_movie_dict         = user_movie_RDD.collect().toMap
+    val user_movie_RDD      = input_data.map(x => (x._1, x._2)).groupByKey().filter(x => x._2.size >= 9)
+    val user_movie_dict     = user_movie_RDD.collect().toMap
 
     // Creating RDD of tuples like ((user1, user2), corated movies) where corated movies >= 9
-    val users_corated_set       = scala.collection.mutable.Set[(Int, Int)]()
-    for (user1_movie <- user_movie_dict){
-      for (user2_movie <- user_movie_dict){
-        if (user1_movie._1 < user2_movie._1){
+    val users_corated_set   = scala.collection.mutable.Set[(Int, Int)]()
+    for (user1_movie <- user_movie_dict)
+    {
+      for (user2_movie <- user_movie_dict)
+      {
+        if (user1_movie._1 < user2_movie._1)
+        {
           val user_intersection = user1_movie._2.toSet.intersect(user2_movie._2.toSet)
-          if (user1_movie._2.toSet.intersect(user2_movie._2.toSet).size >= 9){
+          if (user1_movie._2.toSet.intersect(user2_movie._2.toSet).size >= 9)
+          {
             users_corated_set   += ((user1_movie._1, user2_movie._1))
             users_corated_set   += ((user2_movie._1, user1_movie._1))
           }
